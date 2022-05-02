754f710f1941f49e97b12bd7bb4e7c7869f34d99
Fabio De Pasquale <fabiodpasquale@gmail.com>
2022-05-02 11:47:19 +0200

    feat: code computerPlay random return function, create playRound function

--- script.js
+++ script.js
@@ -1,8 +1,25 @@
-function computerPlay() {
     //randomly return rock, paper or scissors
-    console.log()
+function computerPlay() {
+    // return random number
+    if (Math.floor(Math.random() * 4) == 1) {
+        return "rock";
+    }
+        else if (Math.floor(Math.random() * 4) == 2) { 
+            return "paper";
+        }
+        else {
+            return "scissors";
+        }
+    }
+    //assign r p or s based on ranges
+
+
+function playRound(playerSelection, computerSelection) {
+    switch 
 }
 
-function roundStart(playerSelection, computerSelection) {
-    
-}
+const playerSelection = "rock";
+// case insensitive version - const playerSelection = input.toLowerCase();;
+computerSelection = computerPlay();
+
+console.log(playRound(playerSelection, computerSelection));
