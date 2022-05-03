584b16f3fd22bd77a72b53d826eeca8f600c9a5d
Fabio De Pasquale <fabiodpasquale@gmail.com>
2022-05-03 10:16:00 +0200

    feat!: delete and write new computerPlay, playRound functions

--- script.js
+++ script.js
@@ -1,59 +1,45 @@
-    //randomly return rock, paper or scissors
+let playerSelection;
+let computerSelection = computerPlay();
+let playerScore = 0;
+let computerScore = 0;
+    
+//randomly return random number and assign rock, paper or scissors
 function computerPlay() {
-    // return random number
-    if (Math.floor(Math.random() * 4) == 1) {
-        return "rock";
-    }
-        else if (Math.floor(Math.random() * 4) == 2) { 
-            return "paper";
-        }
-        else {
-            return "scissors";
-        }
-    }
-
-    let playerScore = 0;
-    let computerScore = 0;
-    const computerSelection = computerPlay();
-    const playerSelection = userPrompt();
-    const playedHands = (playerSelection + " " + computerSelection);    
-
-function playRound(playerSelection, computerSelection) {
-    switch (playedHands.toLowerCase()) {
-        case "rock scissors":
-            return "You win! Rock smashes scissors.";
-        case "paper rock":
-            return "You win! Paper covers rock.";
-        case "scissors paper":
-            return "You win! Scissors cut paper.";
-        case ("rock rock" || "paper paper" || "scissors scissors"):
-            return "It's a tie!";
-        default:
-            return "You lose!";
+    let randomNumber = Math.floor(Math.random() * 3) + 1;
+        
+    switch (randomNumber) {
+        case 1: 
+            return "rock";
+        case 2: 
+            return "paper"
+        case 3:
+            return "scissors"
     }
 }
 
-function scoreAdd() {
-    if (playRound() == "You lose!") {
-        computerScore++;
-    } else if (playRound() == "It's a tie!"){
-        return;
+function playRound(computerSelection) {
+    let playerInput = prompt("Throw a hand!");
+    let playerSelection = playerInput.toLowerCase();
+    if (computerSelection === playerSelection) {
+        return "It's a tie!";
+    } else if ((computerSelection === "rock" && playerSelection === "scissors") ||
+        (computerSelection === "paper" && playerSelection === "rock") ||
+        (computerSelection === "scissors" && playerSelection === "paper")) {
+        computerScore = ++computerScore;
+        return `You lose! ${computerSelection} beats ${playerSelection}.`;
     } else {
-        playerScore++;
-        return playerScore;
+        playerScore = ++playerScore;
+        return `You win! ${playerSelection} beats ${computerSelection}.`;
     }
 }
 
-// play 5 rounds calling playRound, keep score and report winner/loser
+// play 5 rounds calling playRound
 function game() {
     for (let i = 0; i < 5; i++) {
         playRound(playerSelection, computerSelection);
-        scoreAdd();
-    } if (playerScore === 5) {
-        return `Congratulations! You won this match with ${playerScore} points.`;
-    } else if (computerScore === 5) {
-        return `Computer won this match with ${playerScore} points. Try again!`
+    } if ((playerScore === 5) || (computerScore === 5)) {
+        return `Match end! ${playerScore} to ${computerScore}`;
     }
 }
 
-console.log(game());
+game();
